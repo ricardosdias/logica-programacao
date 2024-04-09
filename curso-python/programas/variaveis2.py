5fc74b4e036029344623165ea1344c80da22441d
Ricardo Dias <ricardo@ricardodias.org>
2024-04-09 16:49:58 -0300

    adiciona template de exercícios

--- curso-python/programas/variaveis2.py
+++ curso-python/programas/variaveis2.py
@@ -1,18 +1,18 @@
 # Declare uma variável e armazene um valor inteiro.
 
-# Imprima o valor da variável acima e seu tipo.
+# Imprima o valor da variável acima e seu tipo. Rode o programa.
 
 # Agora atribua a esta mesma variável um valor string.
 
-# Imprima o valor da variável e seu tipo.
+# Imprima o valor da variável e seu tipo. Rode o programa.
 
-# Imprima o tipo dos seguintes dados: 10, "10", 7.5, "Olá"
+# Imprima o tipo dos seguintes dados: 10, "10", 7.5, "Olá". Rode o programa.
 
 # Declare uma variável nome e atribua um valor a ela.
 
-# Imprima o id dessa variável.
+# Imprima o id dessa variável. Rode o programa.
 
-# Imprima o id dos seguintes objetos: 10, "10", 7.5, "Olá"
+# Imprima o id dos seguintes objetos: 10, "10", 7.5, "Olá". Rode o programa.
 
 
 '''
