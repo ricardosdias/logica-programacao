741a96ce8b3ed5a873523fd9b0f985e1efe21fcc
Ricardo Dias <ricardo@ricardodias.org>
2024-04-09 16:48:11 -0300

    adiciona template de exercícios

---
+++ curso-python/programas/escopo_variaveis.py
@@ -0,0 +1,68 @@
+# ------------------------------------------------------------------------------
+# Escopo Local
+# ------------------------------------------------------------------------------
+
+# Declare uma função que declara uma variável e imprima ela dentro da função.
+
+# Fora da função, imprima novamente a variável acima.
+
+# Chame a função declarada acima.
+
+# Rode o programa, veja que deu erro, depois comente o comando adicionado acima
+
+# ------------------------------------------------------------------------------
+# Escopo Global:
+# ------------------------------------------------------------------------------
+
+# Declare uma variável global e atribua um valor a ela.
+
+# Declare uma função que imprima a variável global acima.
+
+# Fora da função, imprima novamente a variável acima.
+
+# Chame a função declarada acima.
+
+# Rode o programa
+
+# ------------------------------------------------------------------------------
+# Declaração de variável com mesmo nome fora e dentro de uma função:
+# ------------------------------------------------------------------------------
+# Declare uma variável global e atribua um valor a ela.
+
+# Declare uma função que declara uma variável com o mesmo nome acima e imprima ela dentro da função
+# (mas atribua um valor diferente do valor usado acima).
+
+# Fora da função, imprima novamente a variável acima.
+
+# Chame a função declarada acima.
+
+# Rode o programa
+
+# ------------------------------------------------------------------------------
+# Palavra-chave global:
+# ------------------------------------------------------------------------------
+# Declare uma função que declara uma variável com escopo global.
+
+# Imprima esta variável dentro e fora da função.
+
+# Chame a função declarada acima.
+
+# Rode o programa
+
+# Declare uma nova variável global e atribua um valor a ela.
+
+# Declare uma função que declara uma variável de mesmo nome e atribua a ela um outro valor.
+
+# Imprima esta variável dentro e fora da função.
+
+# Chame a função declarada acima.
+
+# Rode o programa
+
+# Declare uma nova função que altere o valor da variável global declarada acima.
+
+# Imprima esta variável dentro e fora da função.
+
+# Chame a função declarada acima.
+
+# Rode o programa
---
+++ curso-python/programas/variaveis2.py
@@ -0,0 +1,25 @@
+# Declare uma variável e armazene um valor inteiro.
+
+# Imprima o valor da variável acima e seu tipo.
+
+# Agora atribua a esta mesma variável um valor string.
+
+# Imprima o valor da variável e seu tipo.
+
+# Imprima o tipo dos seguintes dados: 10, "10", 7.5, "Olá"
+
+# Declare uma variável nome e atribua um valor a ela.
+
+# Imprima o id dessa variável.
+
+# Imprima o id dos seguintes objetos: 10, "10", 7.5, "Olá"
+
+
+'''
+Os itens abaixo devem ser feitos no shell do python:
+- Declare uma variável chamada número e atribua um valor a ela.
+- Imprima esta variável.
+- Delete esta variável.
+- Imprima novamente ela.
+
+'''
