72ada6548c4ea93cbd9bea59453103c75f0a184e
Ricardo Dias <ricardo@ricardodias.org>
2024-04-11 18:40:55 -0300

    adiciona template de exercicio para numeros

---
+++ curso-python/programas/numeros.py
@@ -0,0 +1,33 @@
+# 1.0 - Crie variáveis de cada tipo numérico e imprima seu tipo para confirmar:
+# 1.1 - Variáveis int
+
+# 1.2 - Variáveis float
+
+# 1.3 - Variáveis complex
+
+# Rode o programa e veja o resultado.
+
+# 1.4 - Declare uma nova variável do tipo complex e imprima seu valor e tipo.
+
+# 1.5 - Imprima a parte real e imaginária da variável complexa acima.
+
+# Rode o programa e veja o resultado.
+
+# 2.0 - Experimente converter entre diferentes tipos numéricos:
+# 2.1 - int para float
+
+# 2.2 - int para complex
+
+# 2.3 - float para int
+
+# 2.4 - float para complex
+
+# Rode o programa e veja o resultado.
+
+# 2.5 - complex para int
+
+# 2.6 - complex para float
+
+# Rode o programa e veja o resultado.
+# Observe que a conversão de complex para int ou float não é possível.
+# Comente o código acima com erro e rode o programa.
