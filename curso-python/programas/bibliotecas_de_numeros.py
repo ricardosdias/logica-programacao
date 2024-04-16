dc6addc4fd25f0b960bad0e3a39903b48188a7d7
Ricardo Dias <ricardo@ricardodias.org>
2024-04-16 19:57:55 -0300

    add files

---
+++ curso-python/programas/bibliotecas_de_numeros.py
@@ -0,0 +1,47 @@
+import math
+import cmath
+import random
+import decimal
+import fractions
+import statistics
+
+# -----------------------------------------------
+# 1.0 Biblioteca math
+# -----------------------------------------------
+
+# 1.1 Use a biblioteca math para calcular a quadrada de 90601.
+print(math.sqrt(90601))
+
+# 1.2 Use a biblioteca math para calcular a raiz cúbica de 185193.
+
+
+# 1.3 Use a biblioteca math para calcular o 47 elevado a 5.
+
+
+# 1.4 Use a biblioteca math para calcular o logaritmo de 1000 na base 10.
+
+
+# 1.5 Use a biblioteca math para imprimir a constante pi.
+
+
+# -----------------------------------------------
+# 2.0 Biblioteca cmath
+# -----------------------------------------------
+
+
+# -----------------------------------------------
+# 3.0 Biblioteca random
+# -----------------------------------------------
+
+
+# -----------------------------------------------
+# 4.0 Biblioteca decimal
+# -----------------------------------------------
+
+# -----------------------------------------------
+# 5.0 Biblioteca fractions
+# -----------------------------------------------
+
+# -----------------------------------------------
+# 6.0 Biblioteca statistics
+# -----------------------------------------------
