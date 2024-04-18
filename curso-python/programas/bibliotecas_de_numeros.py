5e5a8efa6564cc913361b82cf151353c0ae29239
Ricardo Dias <ricardo@ricardodias.org>
2024-04-18 19:48:57 -0300

    add bibliotecas_de_numeros.py

--- curso-python/programas/bibliotecas_de_numeros.py
+++ curso-python/programas/bibliotecas_de_numeros.py
@@ -7,41 +7,138 @@ import statistics
 
 # -----------------------------------------------
 # 1.0 Biblioteca math
+# https://docs.python.org/pt-br/3/library/math.html
 # -----------------------------------------------
 
-# 1.1 Use a biblioteca math para calcular a quadrada de 90601.
-print(math.sqrt(90601))
+# 1.1 Use a biblioteca math para calcular a raiz quadrada de 90601.
+print('raiz quadrada de 90601:', math.sqrt(90601))
 
-# 1.2 Use a biblioteca math para calcular a raiz cúbica de 185193.
-
-
-# 1.3 Use a biblioteca math para calcular o 47 elevado a 5.
+# 1.2 Use a biblioteca math para calcular o 47 elevado a 5.
 
 
-# 1.4 Use a biblioteca math para calcular o logaritmo de 1000 na base 10.
+# 1.3 Use a biblioteca math para calcular a raiz cúbica de 185193.
+# print('raiz cúbica de 185193:', math.pow(185193, 1/3))
+
+# 1.4 Use a biblioteca math para imprimir a constante pi.
 
 
-# 1.5 Use a biblioteca math para imprimir a constante pi.
+# 1.5 Use a biblioteca math para imprimir a constante e.
+
+
+# 1.6 Use a biblioteca arredondar o número 45728.972 para cima com inteiro mais próximo
+
+
+# 1.7 Use a biblioteca arredondar o número 45728.972 para baixo com inteiro mais próximo
+
+
+# 1.8 Use a biblioteca math truncar o número 45728.972
+
+
+# 1.9 Use a biblioteca math para calcular retornar o valor absoluto de -100
 
 
 # -----------------------------------------------
 # 2.0 Biblioteca cmath
+# https://docs.python.org/pt-br/3/library/cmath.html
 # -----------------------------------------------
+
+# 2.1 Use a biblioteca cmath para calcular a raiz quadrada de -1.
+# print('raiz quadrada de -1:', cmath.sqrt(-1))
+
+# 2.2 Use a biblioteca cmath para imprimir a constante pi.
+
+
+# 2.3 Use a biblioteca cmath para imprimir a constante e.
+
+
+# 2.4 Use a biblioteca cmath para calcular a raiz quadrada de -257.
 
 
 # -----------------------------------------------
 # 3.0 Biblioteca random
+# https://docs.python.org/pt-br/3/library/random.html
 # -----------------------------------------------
+
+# 3.1 Use a biblioteca random para gerar um número inteiro aleatório entre 1 e 100.
+# print('número inteiro aleatório entre 1 e 100:', random.randint(1, 100))
+
+# 3.2 Use a biblioteca random para gerar um número real aleatório entre 1 e 100.
+
+
+# 3.3 Use a biblioteca random para gerar um número inteiro aleatório entre 1 e 1000.
+
+
+# 3.4 Use a biblioteca random para gerar um número real aleatório entre 1 e 1000.
+
+
+# 3.5 Use a biblioteca random para embaralhar a lista [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].
+# lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print('lista original:', lista)
+# random.shuffle(lista)
+# print('lista embaralhada:', lista)
+
+# 3.6 Use a biblioteca random para escolher um número aleatório da lista [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].
+# print('minha lista:', lista)
+# print('número aleatório da lista:', random.choice(lista))
+
+# 3.7 Use a biblioteca random para escolher 3 números aleatórios da lista [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].
+
+
+# 3.8 Use a biblioteca random para gerar um numero aleatório entre 1 e 100 com passo de 5.
+
+
+# 3.9 Use a biblioteca random para gerar um numero aleatório par entre 1 e 200 com passo de 10.
 
 
 # -----------------------------------------------
 # 4.0 Biblioteca decimal
+# https://docs.python.org/pt-br/3/library/decimal.html
 # -----------------------------------------------
+
+# 4.1 Use a biblioteca math para imprimir a raiz quadrada de 2
+# print('raiz quadrada de 2 pela biblioteca math:', math.sqrt(2))
+
+# 4.2 Agora use a biblioteca decimal para imprimir a raiz quadrada de 2
+# print('raiz quadrada de 2 pela biblioteca decimal:', decimal.Decimal(2).sqrt())
+
+# 4.3 Use a biblioteca decimal para imprimir a raiz quadrada de 2 com precisão de 5
+
+# 4.3 Use a biblioteca decimal para imprimir a raiz quadrada de 57 com precisão de 3
+
 
 # -----------------------------------------------
 # 5.0 Biblioteca fractions
+# https://docs.python.org/pt-br/3/library/fractions.html
 # -----------------------------------------------
+
+# 5.1 Use a biblioteca fractions para criar a fração 3/4
+# print('fração 3/4:', fractions.Fraction(3, 4))
+
+# 5.2 Use a biblioteca fractions para criar a fração 2/3
+
+# 5.3 Imprima a soma das frações 3/4 e 2/3
+
+# 5.4 Imprima o numerador da fração 3/4
+
+# 5.5 Imprima o denominador da fração 3/4
+
+# 5.6 Imprima a fração equivalente ao número real 0.125
+
+# 5.7 Imprima a fração equivalente a string '0.25'
+
 
 # -----------------------------------------------
 # 6.0 Biblioteca statistics
+# https://docs.python.org/pt-br/3/library/statistics.html
 # -----------------------------------------------
+
+# 6.1 Use a biblioteca statistics para calcular a média da lista [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print('média da lista [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:', statistics.mean([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+
+# 6.2 Use a biblioteca statistics para calcular a mediana da lista [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# 6.3 Use a biblioteca statistics para calcular a moda da lista [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# 6.4 Use a biblioteca statistics para calcular a variância da lista [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# 6.5 Use a biblioteca statistics para calcular o desvio padrão da lista [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
