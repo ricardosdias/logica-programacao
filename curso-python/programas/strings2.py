11a2376fa07b3502e4cb08210008a0b498af067c
Ricardo Dias <ricardo@ricardodias.org>
2024-05-14 18:44:13 -0300

    add strings2 script

---
+++ curso-python/programas/strings2.py
@@ -0,0 +1,38 @@
+# 1.0 - Convertendo para o Formato String
+n1 = 10
+n2 = 20.5
+l1 = [1, 2, 3]
+
+# 1.1 - Imprima o valor e o tipo das variáveis n1, n2 e l1
+# Exemplo de resposta: print(f"n1 = {n1} e seu tipo é {type(n1)}")
+
+# 1.2 - Converta as variáveis n1, n2 e l1 para o formato string
+# Exemplo de resposta: print(f"n1 = {str(n1)} e seu tipo é {type(str(n1))}")
+
+# 2.0 - Escape Caracteres
+
+# 2.1 - Declare uma variável string, use o caractere de escape para pular uma linha e imprima a string
+# Exemplo de resposta: print("Olá\nmundo!")
+
+# 2.2 - Declare uma variável string, use o caractere de escape para tabular e imprima a string
+
+# 2.3 - Declare uma variável string, use o caractere de escape de retorno de carro e imprima a string
+
+# 2.4 - Declare uma variável string, use o caractere de backspace e imprima a string
+
+# 2.5 - Declare uma variável string, use o caractere de escape para tabular verticalmente e imprima a string
+
+# 3.0 - Operadores com Strings
+
+# 3.1 - Concatene as strings "Olá" e "Mundo" e imprima o resultado
+
+# 3.2 - Concatene as strings "Olá" e "Mundo" com um espaço entre elas e imprima o resultado
+
+# 3.3 - Concatene as strings "Olá" e "Mundo" com um tab entre elas e imprima o resultado
+
+# 3.4 - Concatene as strings "Olá" e "Mundo" com 100 espaços entre elas e imprima o resultado
+# Use o operador de multiplicação para repetir o espaço
+
+# 3.5 - Verifique se a string "Olá" está presente na string "Olá, Mundo!" e imprima o resultado
+
+# 3.6 - Verifique se a string "Python" está presente na string "Olá, Mundo!" e imprima o resultado
