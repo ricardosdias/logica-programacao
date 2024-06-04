def03b6bb59e00fd569d56586f835d741ac5d412
Ricardo Dias <ricardo@ricardodias.org>
2024-06-04 18:59:43 -0300

    add operadores_membros script

---
+++ curso-python/programas/operadores_membros.py
@@ -0,0 +1,42 @@
+# ---------------------------------------------------
+# Operadores de Membros
+# ---------------------------------------------------
+print('Seção 1: Strings')
+print('Exercício 1: Verificar se uma palavra está em uma frase')
+frase = "Python é uma linguagem de programação"
+print('Frase:', frase)
+
+print('Exercício 1.1: Verificar se uma palavra "Python" está na frase definida acima')
+
+print('Exercício 1.2: Verificar se uma palavra "Java" está na frase definida acima')
+
+print('Exercício 1.3: Verificar se uma palavra "Python" não está na frase definida acima')
+
+print('Exercício 1.4: Verificar se uma palavra "Java" não está na frase definida acima')
+
+
+print('\nSeção 2: Listas')
+print('Exercício 2: Verificar se uma elemento está em uma lista')
+lista = [1, 2, 3, 4, 5]
+print('Lista:', lista)
+
+print('Exercício 2.1: Verificar se o número 3 está na lista definida acima')
+
+print('Exercício 2.2: Verificar se o número 6 está na lista definida acima')
+
+print('Exercício 2.3: Verificar se o número 3 não está na lista definida acima')
+
+print('Exercício 2.4: Verificar se o número 6 não está na lista definida acima')
+
+print('\nSeção 3: Dicionário')
+print('Exercício 3: Verificar se uma elemento está em um dicionário')
+dicionario = {"nome": "João", "idade": 30}
+print('Dicionário:', dicionario)
+
+print('Exercício 3.1: Verificar se a chave "nome" está no dicionário definido acima')
+
+print('Exercício 3.2: Verificar se a chave "email" está no dicionário definido acima')
+
+print('Exercício 3.3: Verificar se a chave "nome" não está no dicionário definido acima')
+
+print('Exercício 3.4: Verificar se a chave "email" não está no dicionário definido acima')
