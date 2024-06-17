c785abcd462370d07c67f03fc481290a6463c145
Ricardo Dias <ricardo@ricardodias.org>
2024-06-17 20:19:26 -0300

    operadores_bitwise.py

---
+++ curso-python/programas/operadores_bitwise.py
@@ -0,0 +1,95 @@
+# ---------------------------------------------------
+# Operadores Bitwise
+# ---------------------------------------------------
+
+print('Exercício 1: AND Bitwise')
+# Dado dois números inteiros a e b, escreva uma função
+# que retorne o resultado da operação AND bitwise entre eles.
+
+
+def bitwise_and(a, b):
+    # Troque o valor None pelo seu código
+    return None
+
+
+print('bitwise_and(5, 3):', bitwise_and(5, 3))  # 1
+print('bitwise_and(8, 2):', bitwise_and(8, 2))  # 0
+print('bitwise_and(7, 1):', bitwise_and(7, 1))  # 1
+
+print('\nExercício 2: OR Bitwise')
+# Dado dois números inteiros a e b, escreva uma função
+# que retorne o resultado da operação OR bitwise entre eles.
+
+
+def bitwise_or(a, b):
+    # Troque o valor None pelo seu código
+    return None
+
+
+print('bitwise_or(5, 3):', bitwise_or(5, 3))  # 7
+print('bitwise_or(8, 2):', bitwise_or(8, 2))  # 10
+print('bitwise_or(7, 1):', bitwise_or(7, 1))  # 7
+
+
+print('\nExercício 3: XOR Bitwise')
+# Dado dois números inteiros a e b, escreva uma função
+# que retorne o resultado da operação XOR bitwise entre eles.
+
+
+def bitwise_xor(a, b):
+    # Troque o valor None pelo seu código
+    return None
+
+
+print('bitwise_xor(5, 3):', bitwise_xor(5, 3))  # 6
+print('bitwise_xor(8, 2):', bitwise_xor(8, 2))  # 10
+print('bitwise_xor(7, 1):', bitwise_xor(7, 1))  # 6
+
+print('\nExercício 4: Complemento de 1 Bitwise')
+# Dado um número inteiro a, escreva uma função
+# que retorne o resultado do complemento de 1 bitwise.
+
+
+def bitwise_complemento_1(a):
+    # Troque o valor None pelo seu código
+    return None
+
+
+print('bitwise_complemento_1(5):', bitwise_complemento_1(5))  # -6
+print('bitwise_complemento_1(8):', bitwise_complemento_1(8))  # -9
+print('bitwise_complemento_1(7):', bitwise_complemento_1(7))  # -8
+
+
+print('\nExercício 5: Deslocamento à Esquerda')
+# Dado dois números inteiros a e b, escreva uma função
+# que retorne o resultado do deslocamento à esquerda de a por b bits.
+
+
+def bitwise_deslocamento_esquerda(a, b):
+    # Troque o valor None pelo seu código
+    return None
+
+
+print('bitwise_deslocamento_esquerda(5, 1):',
+      bitwise_deslocamento_esquerda(5, 1))  # 10
+print('bitwise_deslocamento_esquerda(8, 2):',
+      bitwise_deslocamento_esquerda(8, 2))  # 32
+print('bitwise_deslocamento_esquerda(7, 1):',
+      bitwise_deslocamento_esquerda(7, 1))  # 14
+
+print('\nExercício 6: Deslocamento à Direita')
+# Dado dois números inteiros a e b, escreva uma função
+# que retorne o resultado do deslocamento à direita de a por b bits.
+
+
+def bitwise_deslocamento_direita(a, b):
+    # Troque o valor None pelo seu código
+    return None
+
+
+print('bitwise_deslocamento_direita(5, 1):',
+      bitwise_deslocamento_direita(5, 1))  # 2
+print('bitwise_deslocamento_direita(8, 2):',
+      bitwise_deslocamento_direita(8, 2))  # 2
+print('bitwise_deslocamento_direita(7, 1):',
+      bitwise_deslocamento_direita(7, 1))  # 3
