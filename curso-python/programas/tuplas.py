ffe8e2c2b2f20579bddcfcfaf9a2fd8442c0a9d6
Ricardo Dias <ricardo.dias@ab-inbev.com>
2024-07-16 18:48:58 -0300

    add tuplas.py

---
+++ curso-python/programas/tuplas.py
@@ -0,0 +1,232 @@
+# ---------------------------------------------------
+# Tuplas
+# ---------------------------------------------------
+
+from collections import namedtuple
+import random
+print('\nExercício 01:\n'
+      'Crie uma tupla com os números 2, 4, 6 e 8 e imprima o resultado na tela.')
+print('Resultado esperado: (2, 4, 6, 8)')
+print('Resultado obtido:')
+# ESCREVA SEU CÓDIGO AQUI
+
+# Comente o código abaixo - exit - para prosseguir com os exercícios
+exit()
+print('\nExercício 02:\n'
+      'Certifique-se de que a tupla é realmente uma tupla.')
+print('Resultado esperado: True')
+print('Resultado obtido:')
+# ESCREVA SEU CÓDIGO AQUI
+
+# Comente o código abaixo - exit - para prosseguir com os exercícios
+exit()
+print('\nExercício 03:\n'
+      'No Shell do python crie uma tupla com os números 1, 3, 5 e 7.\n'
+      'Atribua essa tupla a uma variável chamada t.\n'
+      'Tente alterar o primeiro elemento da tupla para 2.')
+print('Resultado esperado: TypeError')
+
+
+# Comente o código abaixo - exit - para prosseguir com os exercícios
+exit()
+print('\nExercício 04:\n'
+      'No Shell do python crie uma lista com os números 1, 3, 5 e 7.\n'
+      'Atribua essa lista a uma variável chamada l.\n'
+      'Tente alterar o primeiro elemento da lista para 2.\n')
+print('Resultado esperado: [2, 3, 5, 7]')
+
+# Comente o código abaixo - exit - para prosseguir com os exercícios
+exit()
+print('\nExercício 05:\n'
+      'Crie uma tupla com nome de cidades e atribua a variável cidades.\n'
+      'Use parênteses para criar a tupla.\n'
+      'Imprima o resultado na tela.')
+# ESCREVA SEU CÓDIGO AQUI
+
+# Comente o código abaixo - exit - para prosseguir com os exercícios
+exit()
+print('\nExercício 06:\n'
+      'Crie uma tupla com nome de estados e atribua a variável estados.\n'
+      'Não use parênteses para criar a tupla.\n'
+      'Imprima o resultado na tela.')
+# ESCREVA SEU CÓDIGO AQUI
+
+
+# Comente o código abaixo - exit - para prosseguir com os exercícios
+exit()
+print('\nExercício 07:\n'
+      'Crie uma tupla vazia usando uma das sintaxes vistas na aula.\n'
+      'Imprima o resultado na tela.\n'
+      'Certifique-se de que a tupla é realmente uma tupla.')
+print('Resultado esperado: () e True')
+print('Resultado obtido:')
+# ESCREVA SEU CÓDIGO AQUI
+
+# Comente o código abaixo - exit - para prosseguir com os exercícios
+exit()
+print('\nExercício 08:\n'
+      'Crie uma tupla vazia usando uma das sintaxes vistas na aula.\n'
+      'mas que seja diferente da sintaxe usada no exercício anterior.\n'
+      'Imprima o resultado na tela.\n'
+      'Certifique-se de que a tupla é realmente uma tupla.')
+print('Resultado esperado: () e True')
+print('Resultado obtido:')
+# ESCREVA SEU CÓDIGO AQUI
+
+# Comente o código abaixo - exit - para prosseguir com os exercícios
+exit()
+print('\nExercício 09:\n'
+      'Crie uma tupla com 2 nomes de animais.\n'
+      'Imprima o resultado na tela.\n'
+      'Certifique-se de que a tupla é realmente uma tupla.')
+print('Resultado esperado: ("gato", "cachorro") e True')
+print('Resultado obtido:')
+# ESCREVA SEU CÓDIGO AQUI
+
+
+# Comente o código abaixo - exit - para prosseguir com os exercícios
+exit()
+print('\nExercício 10:\n'
+      'Crie uma tupla com apenas seu nome.\n'
+      'Imprima o resultado na tela.\n'
+      'Certifique-se de que a tupla é realmente uma tupla.')
+# ESCREVA SEU CÓDIGO AQUI
+
+# Comente o código abaixo - exit - para prosseguir com os exercícios
+exit()
+print('\nExercício 11:\n'
+      'Crie uma tupla chamada dados_pessoais com seu nome, idade e cidade.\n'
+      'Acesse e imprima o segundo elemento da tupla dados_pessoais.')
+print('Resultado esperado: SUA IDADE')
+print('Resultado obtido:')
+# ESCREVA SEU CÓDIGO AQUI
+
+# Comente o código abaixo - exit - para prosseguir com os exercícios
+exit()
+print('\nExercício 12:\n'
+      'Altere o elemento cidade da tupla dados_pessoais para o país que você mora.\n'
+      'Imprima o resultado na tela.')
+# ESCREVA SEU CÓDIGO AQUI
+
+# Comente o código abaixo - exit - para prosseguir com os exercícios
+exit()
+print('\nExercício 13:\n'
+      'Crie uma tupla com e frutas e imprima o resultado na tela.\n'
+      'Em seguida, converta a tupla em uma lista e imprima o resultado na tela.\n'
+      'Em seguida, adicione uma fruta à lista e imprima o resultado na tela.\n'
+      'Em seguida, converta a lista em uma tupla e imprima o resultado na tela.')
+# ESCREVA SEU CÓDIGO AQUI
+
+
+# Comente o código abaixo - exit - para prosseguir com os exercícios
+exit()
+print('\nExercício 14:\n'
+      'Crie uma tupla com os números 1, 3, 5 e 7 e imprima o '
+      'primeiro e último elemento da tupla na tela.')
+print('Resultado esperado: 1 e 7')
+print('Resultado obtido:')
+# ESCREVA SEU CÓDIGO AQUI
+
+# Comente o código abaixo - exit - para prosseguir com os exercícios
+exit()
+print('\nExercício 15:\n'
+      'Crie uma tupla com 4 elementos nomes de frutas.\n'
+      'e atribua essa tupla a uma variável chamada frutas.\n'
+      'Imprima o resultado na tela.')
+# ESCREVA SEU CÓDIGO AQUI
+
+# Comente o código abaixo - exit - para prosseguir com os exercícios
+exit()
+print('\nExercício 16:\n'
+      'Altere a tupla de frutas para incluir mais uma fruta.\n'
+      'Imprima o resultado na tela.')
+# ESCREVA SEU CÓDIGO AQUI
+
+# Comente o código abaixo - exit - para prosseguir com os exercícios
+exit()
+print('\nExercício 17:\n'
+      'Altere a tupla de frutas para remover o primeiro elemento da tupla.\n'
+      'Imprima o resultado na tela.')
+# ESCREVA SEU CÓDIGO AQUI
+
+
+# Comente o código abaixo - exit - para prosseguir com os exercícios
+exit()
+print('\nExercício 18:\n'
+      'Use a instrução for para imprimir todos os itens da tupla abaixo.')
+lista = ('Natasha', 'Tony', 'Steve', 'Bruce', 'Thor')
+# ESCREVA SEU CÓDIGO AQUI
+
+
+# Comente o código abaixo - exit - para prosseguir com os exercícios
+exit()
+print('\nExercício 19:\n'
+      'Verifique se os números 100, 200 e 300 estão presentes na tupla abaixo.\n'
+      'Imprima a tupla e o resultado na tela.')
+numeros_aleatorios = tuple([random.randint(0, 1000) for _ in range(300)])
+print('Resultado esperado: True ou False, True ou False, True ou False')
+print('Resultado obtido:')
+# ESCREVA SEU CÓDIGO AQUI
+
+
+# Comente o código abaixo - exit - para prosseguir com os exercícios
+exit()
+print('\nExercício 20:\n'
+      'Crie uma tupla com 3 nomes masculinos e outra tupla com 3 nomes femininos.\n'
+      'Em seguida, crie uma tupla que seja a concatenação das duas tuplas anteriores.\n'
+      'Imprima o resultado na tela.')
+# ESCREVA SEU CÓDIGO AQUI
+
+
+# Comente o código abaixo - exit - para prosseguir com os exercícios
+exit()
+print('\nExercício 21:\n'
+      'Crie uma tupla com dois números inteiros.\n'
+      'Em seguida, crie uma tupla que seja a repetição da tupla acima 10 vezes.\n'
+      'Imprima o resultado na tela.')
+# ESCREVA SEU CÓDIGO AQUI
+
+
+# Comente o código abaixo - exit - para prosseguir com os exercícios
+exit()
+print('\nExercício 22:\n'
+      'Após gerar a tupla abaixo (numeros_aleatorios)\n'
+      'Mostre a quantidade de elementos da tupla, o maior valor e o menor valor.\n'
+      'Também mostre quantas vezes o número 2 aparece na tupla.')
+numeros_aleatorios = tuple([random.randint(0, 1000) for _ in range(300)])
+# ESCREVA SEU CÓDIGO AQUI
+
+
+# Comente o código abaixo - exit - para prosseguir com os exercícios
+exit()
+print('\nExercício 23:\n'
+      'Crie uma tupla com 3 elementos e desempacote a tupla em 3 variáveis.\n'
+      'Imprima o resultado na tela.')
+# ESCREVA SEU CÓDIGO AQUI
+
+# Comente o código abaixo - exit - para prosseguir com os exercícios
+exit()
+print('\nExercício 24:\n'
+      'Crie uma tupla com 3 elementos e desempacote a tupla em 2 variáveis.\n'
+      'Sendo a segunda variável uma tupla com os elementos restantes.\n'
+      'Imprima o resultado na tela.')
+# ESCREVA SEU CÓDIGO AQUI
+
+# Comente o código abaixo - exit - para prosseguir com os exercícios
+exit()
+print('\nExercício 25:\n'
+      'Crie uma função que receba dois números e retorne a soma e a multiplicação dos números.\n'
+      'A função deve retornar uma tupla com a soma e a multiplicação dos números.\n'
+      'Imprima o resultado na tela.')
+print('Resultado esperado: (soma, multiplicacao)')
+print('Resultado obtido:')
+# ESCREVA SEU CÓDIGO AQUI
+
+# Comente o código abaixo - exit - para prosseguir com os exercícios
+exit()
+print('\nExercício 26:\n'
+      'Crie uma Named Tuple com os campos nome, idade e cidade.\n'
+      'Atribua valores a cada campo e imprima o resultado na tela.')
+print('Resultado esperado: nome, idade, cidade')
+print('Resultado obtido:')
+# ESCREVA SEU CÓDIGO AQUI
